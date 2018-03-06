c1e86fee9b63c7e97946fd5f77e15d87a04fa84b
GedasFX <gediminasmilasius@gmail.com>
2018-03-06 19:10:19 +0200

    Remove UserDbContext

---
+++ Kodhier/Controllers/Admin/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodhier.Controllers.Admin
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

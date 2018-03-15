953fe77000d358c8e15cf9bbe866e554b1a500d0
GedasFX <gedasfx@github.com>
2018-03-15 18:04:30 +0200

    Add Kodhier.Tests Get Moq from nuget

---
+++ Kodhier.Tests/OrderControllerTests.cs
@@ -0,0 +1,16 @@
+using Kodhier.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Kodhier.Tests
+{
+    [TestClass]
+    public class OrderControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var mockRepo = new Mock<KodhierDbContext>();
+        }
+    }
+}

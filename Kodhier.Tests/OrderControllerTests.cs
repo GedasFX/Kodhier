4827581d3b31c27479acd92894ecaa00aa541af1
GedasFX <gedasfx@github.com>
2018-03-16 16:05:43 +0200

    Update 2 Unit test fields.

--- Kodhier.Tests/OrderControllerTests.cs
+++ Kodhier.Tests/OrderControllerTests.cs
@@ -1,10 +1,15 @@
 using Kodhier.Controllers;
 using Kodhier.Data;
 using Kodhier.Models;
+using Kodhier.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,21 +17,33 @@ namespace Kodhier.Tests
 {
     public class OrderControlerTests
     {
-        KodhierDbContext _mock = new KodhierDbContext(new DbContextOptionsBuilder<KodhierDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
+        KodhierDbContext _mock;
+
+        OrderController _controller;
 
         public OrderControlerTests()
         {
+            try { AutoMapperConfig.RegisterMappings(); } catch { };
+
+            _mock = new KodhierDbContext(new DbContextOptionsBuilder<KodhierDbContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options);
             GenMockData();
+
+            var m_User = new Mock<ClaimsPrincipal>();
+            m_User.Setup(t => t.Claims).Returns(new[] { new Claim(ClaimTypes.NameIdentifier, "ec04ee08-f434-41d0-a208-15bd2dcb3389") });
+            _controller = new OrderController(_mock)
+            {
+                ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext { User = m_User.Object } }
+            };
         }
 
         private void GenMockData()
         {
-            var uId = Guid.NewGuid();
+            var uId = new Guid("ec04ee08-f434-41d0-a208-15bd2dcb3389");
             var pId = Guid.NewGuid();
             var user = new ApplicationUser() { Id = uId.ToString(), Age = new DateTime(), Email = "asdf@jkl.lt", FirstName = "ASdF", UserName = "AwDcV" };
             _mock.Users.Add(user);
             _mock.Users.Add(new ApplicationUser() { Id = Guid.NewGuid().ToString(), Age = new DateTime(), Email = "dummy@jkdl.lt", FirstName = "dummy", UserName = "dumb" });
-            _mock.Pizzas.Add(new Pizza() { Creator = user, Id = pId, Name = "Havaian", Price = 46.5M, Size = 46 });
+            _mock.Pizzas.Add(new Pizza() { Creator = user, Id = pId, Name = "Havaian", Price = 46.5M });
             _mock.SaveChangesAsync();
         }
 
@@ -35,9 +52,8 @@ namespace Kodhier.Tests
         {
             var pizza = _mock.Pizzas.Single(e => e.Name == "Havaian");
             var user = _mock.Users.Single(e => e.UserName == "AwDcV");
-            var order = new Order() { Pizza = pizza, Client = user, Quantity = 3 };
-            var controller = new OrderController(_mock);
-            var res = await controller.Create(order);
+            var order = new OrderViewModel() { Pizza = pizza, Quantity = 3, Size = 5 };
+            var res = await _controller.Create(pizza.Id, new OrderCreateViewModel { Order = order });
 
             Assert.IsType<RedirectToActionResult>(res);
             Assert.Equal("Index", ((RedirectToActionResult)res).ActionName);
@@ -48,9 +64,10 @@ namespace Kodhier.Tests
         {
             var pizza = _mock.Pizzas.Single(e => e.Name == "Havaian");
             var user = _mock.Users.Single(e => e.UserName == "AwDcV");
-            var order = new Order() { Pizza = pizza, Client = user, Quantity = -9 };
-            var controller = new OrderController(_mock);
-            var res = await controller.Create(order);
+            var order = new OrderViewModel() { Pizza = pizza, Quantity = -9 };
+            if (!Validator.TryValidateObject(order, new ValidationContext(order), null, true));
+                _controller.ModelState.AddModelError("err", "Error");
+            var res = await _controller.Create(pizza.Id, new OrderCreateViewModel { Order = order });
 
             Assert.IsType<ViewResult>(res);
         }

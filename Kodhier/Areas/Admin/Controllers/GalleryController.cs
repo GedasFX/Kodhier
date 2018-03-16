07c074fd57f147868357e9d1f399c6bc53e3376a
linsas <linsas0109@gmail.com>
2018-03-16 15:31:26 +0200

    Add image upload to gallery

--- Kodhier/Areas/Admin/Controllers/GalleryController.cs
+++ Kodhier/Areas/Admin/Controllers/GalleryController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kodhier.Areas.Admin.Controllers
@@ -9,8 +12,39 @@ namespace Kodhier.Areas.Admin.Controllers
     [Area("Admin")]
     public class GalleryController : Controller
     {
+        private readonly IHostingEnvironment _env;
+
+        public GalleryController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile imgfile)
+        {
+            if (imgfile.Length > 512000)
+            {
+                ModelState.AddModelError(string.Empty, "Max file size 512 KB");
+                return View();
+            }
+
+            if (imgfile.ContentType != "text/jpeg")
+            {
+                ModelState.AddModelError(string.Empty, "Only images allowed");
+                return View();
+            }
+            
+            var filePath = Path.Combine(_env.WebRootPath, "uploads/img/gallery/", Path.GetFileName(imgfile.FileName));
+            using (var stream = new FileStream(filePath, FileMode.Create)) // might be other errors?
+            {
+                await imgfile.CopyToAsync(stream);
+            }
+            
             return View();
         }
     }

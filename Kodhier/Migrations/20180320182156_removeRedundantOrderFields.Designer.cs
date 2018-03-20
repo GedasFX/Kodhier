905e56b03171b9fbeb653f2116b705143db82956
GedasFX <gedasfx@github.com>
2018-03-20 20:36:48 +0200

    Update the OrderStatus

--- Kodhier/Migrations/20180320182156_removeRedundantOrderFields.Designer.cs
+++ Kodhier/Migrations/20180320182156_removeRedundantOrderFields.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Kodhier.Migrations
 {
     [DbContext(typeof(KodhierDbContext))]
-    partial class KodhierDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180320182156_removeRedundantOrderFields")]
+    partial class removeRedundantOrderFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

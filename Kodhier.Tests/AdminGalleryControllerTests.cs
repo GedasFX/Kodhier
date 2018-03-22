f588162696d296e05de723230658cc511cfc17d7
linsas <linsas0109@gmail.com>
2018-03-22 20:06:00 +0200

    add 2 more tests

--- Kodhier.Tests/AdminGalleryControllerTests.cs
+++ Kodhier.Tests/AdminGalleryControllerTests.cs
@@ -24,41 +24,102 @@ namespace Kodhier.Tests
             mockEnvironment.Setup(m => m.WebRootPath).Returns(testRootDir);
 
             _controller = new GalleryController(mockEnvironment.Object);
-        }
-        
-        [Fact(DisplayName = "Normal PNG upload")]
-        public async Task Upload_Something_Ok()
-        {
-            // arrange
-            var testFile = new Mock<IFormFile>();
-            var sourceFile = File.OpenRead(testRootDir + "/test.png");
+		}
 
-            var memStream = new MemoryStream();
-            var writer = new StreamWriter(memStream);
-            writer.Write(sourceFile);
-            writer.Flush();
-            memStream.Position = 0;
+		[Fact(DisplayName = "Gallery upload normal PNG")]
+		public async Task Upload_Something_Ok()
+		{
+			// arrange
+			var testFile = new Mock<IFormFile>();
+			var sourceFile = File.OpenRead(testRootDir + "/test.png");
 
-            var fileName = sourceFile.Name;
-            var fileType = "image/png";
-            long fileSize = 500_000;
+			var memStream = new MemoryStream();
+			var writer = new StreamWriter(memStream);
+			writer.Write(sourceFile);
+			writer.Flush();
+			memStream.Position = 0;
 
-            testFile.Setup(f => f.Length).Returns(fileSize).Verifiable();
-            testFile.Setup(f => f.ContentType).Returns(fileType).Verifiable();
-            testFile.Setup(f => f.FileName).Returns(fileName).Verifiable();
-            testFile.Setup(_ => _.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
-                .Returns((Stream stream, CancellationToken token) => memStream.CopyToAsync(stream))
-                .Verifiable();
+			var fileName = sourceFile.Name;
+			var fileType = "image/png";
+			long fileSize = 500_000;
 
-            // act
-            var inputFile = testFile.Object;
-            var result = await _controller.Upload(inputFile);
+			testFile.Setup(f => f.Length).Returns(fileSize);
+			testFile.Setup(f => f.ContentType).Returns(fileType);
+			testFile.Setup(f => f.FileName).Returns(fileName);
+			testFile.Setup(_ => _.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+				.Returns((Stream stream, CancellationToken token) => memStream.CopyToAsync(stream));
 
-            // assert
-            testFile.Verify();
-			
+			// act
+			var inputFile = testFile.Object;
+			var result = await _controller.Upload(inputFile);
+
+			// assert
 			var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
 			Assert.Equal("Index", redirectToActionResult.ActionName);
-        }
-    }
+		}
+
+		[Fact(DisplayName = "Gallery upload too large")]
+		public async Task Upload_Too_Large()
+		{
+			// arrange
+			var testFile = new Mock<IFormFile>();
+			var sourceFile = File.OpenRead(testRootDir + "/test.png");
+
+			var memStream = new MemoryStream();
+			var writer = new StreamWriter(memStream);
+			writer.Write(sourceFile);
+			writer.Flush();
+			memStream.Position = 0;
+
+			var fileName = sourceFile.Name;
+			var fileType = "image/png";
+			long fileSize = 520_000;
+
+			testFile.Setup(f => f.Length).Returns(fileSize);
+			testFile.Setup(f => f.ContentType).Returns(fileType);
+			testFile.Setup(f => f.FileName).Returns(fileName);
+			testFile.Setup(_ => _.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+				.Returns((Stream stream, CancellationToken token) => memStream.CopyToAsync(stream));
+
+			// act
+			var inputFile = testFile.Object;
+			var result = await _controller.Upload(inputFile);
+
+			// assert
+			var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+			Assert.Equal("Index", redirectToActionResult.ActionName);
+		}
+
+		[Fact(DisplayName = "Gallery upload not an image")]
+		public async Task Upload_Not_Image()
+		{
+			// arrange
+			var testFile = new Mock<IFormFile>();
+			var sourceFile = File.OpenRead(testRootDir + "/test.txt");
+
+			var memStream = new MemoryStream();
+			var writer = new StreamWriter(memStream);
+			writer.Write(sourceFile);
+			writer.Flush();
+			memStream.Position = 0;
+
+			var fileName = sourceFile.Name;
+			var fileType = "text/plain";
+			long fileSize = 5_000;
+
+			testFile.Setup(f => f.Length).Returns(fileSize);
+			testFile.Setup(f => f.ContentType).Returns(fileType);
+			testFile.Setup(f => f.FileName).Returns(fileName);
+			testFile.Setup(_ => _.CopyToAsync(It.IsAny<Stream>(), It.IsAny<CancellationToken>()))
+				.Returns((Stream stream, CancellationToken token) => memStream.CopyToAsync(stream));
+
+			// act
+			var inputFile = testFile.Object;
+			var result = await _controller.Upload(inputFile);
+
+			// assert
+			var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+			Assert.Equal("Index", redirectToActionResult.ActionName);
+		}
+	}
 }

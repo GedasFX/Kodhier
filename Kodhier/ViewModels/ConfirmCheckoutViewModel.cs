dc02c7b4fbc8b281238c16b09fa9156c1538ada5
GedasFX <gedasfx@github.com>
2018-04-21 16:45:11 +0300

    Map changes. Autofill /Order/Checkout

--- Kodhier/ViewModels/ConfirmCheckoutViewModel.cs
+++ Kodhier/ViewModels/ConfirmCheckoutViewModel.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Kodhier.ViewModels
 {
     public class ConfirmCheckoutViewModel
     {
-		public String ConfirmAddress { get; set; }
+        [Required]
+		public string ConfirmAddress { get; set; }
 
 		public decimal Price { get; set; }
 		public IEnumerable<CheckoutViewModel> CheckoutList { get; set; }

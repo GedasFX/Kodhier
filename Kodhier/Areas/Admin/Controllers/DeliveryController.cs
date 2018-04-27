f7b509bc10d647f926acb6dbe9b2f113b20aa23d
linsas <linsas0109@gmail.com>
2018-04-27 13:16:10 +0300

    Add list of pizzas to be delivered

---
+++ Kodhier/Areas/Admin/Controllers/DeliveryController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kodhier.Data;
+using Kodhier.Extensions;
+using Kodhier.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kodhier.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Authorize(Roles = "Admin")]
+	public class DeliveryController : Controller
+	{
+		private readonly KodhierDbContext _context;
+
+		public DeliveryController(KodhierDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var clientId = User.GetId();
+
+			// check carefully
+			var orders = _context.Orders
+				.Where(o => o.Client.Id == clientId)
+				.Where(o => o.Status == Models.OrderStatus.Delivering)
+				.OrderByDescending(c => c.PlacementDate)
+				.Select(o => new DeliveryViewModel
+				{
+					Id = o.Id,
+					Quantity = o.Quantity,
+					Size = o.Size,
+					Comment = o.Comment,
+					Name = o.Pizza.Name,
+					ImagePath = o.Pizza.ImagePath,
+					DeliveryAddress = o.DeliveryAddress
+				});
+			return View(orders);
+		}
+    }
+}

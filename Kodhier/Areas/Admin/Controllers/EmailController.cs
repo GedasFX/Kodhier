d7f8787ba4fd561606ca89bb7d2d58a2f8dcd3a0
GedasFX <gedasfx@github.com>
2018-05-11 15:47:21 +0300

    Send promotional emails

--- Kodhier/Areas/Admin/Controllers/EmailController.cs
+++ Kodhier/Areas/Admin/Controllers/EmailController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Kodhier.Data;
+using Kodhier.Models;
+using Kodhier.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Kodhier.Areas.Admin.Controllers
@@ -7,10 +12,45 @@ namespace Kodhier.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class EmailController : Controller
     {
+        private readonly KodhierDbContext _context;
+        private readonly IEmailSender _emailSender;
+
+        public EmailController(KodhierDbContext context, IEmailSender emailSender)
+        {
+            _context = context;
+            _emailSender = emailSender;
+        }
+
         [ActionName("Index")]
         public IActionResult Email()
         {
-            return View(null);
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SendEmail(string subject, string htmlMessage)
+        {
+            await SendAll(subject, htmlMessage);
+            return RedirectToAction("Index");
+        }
+
+        private async Task SendAll(string subject, string message)
+        {
+            var users = _context.Users
+                .Where(u => u.EmailConfirmed)
+                .Where(u => u.EmailSendPromotional);
+            foreach (var user in users)
+            {
+                await SendEmail(user, subject, message);
+            }
+        }
+
+        private async Task SendEmail(ApplicationUser user, string subject, string message)
+        {
+            var messageBody = string.Format(message, user.UserName);
+
+            await _emailSender.SendEmailAsync(user.Email, subject, messageBody);
         }
     }
 }

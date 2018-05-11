77a1d2e8ff64bafd7295a0f4ad1c738b7fb2a7df
GedasFX <gedasfx@github.com>
2018-05-11 15:10:46 +0300

    Add text editor to email

--- Kodhier/Areas/Admin/Controllers/EmailController.cs
+++ Kodhier/Areas/Admin/Controllers/EmailController.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Kodhier.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class EmailController : Controller
     {
         [ActionName("Index")]
         public IActionResult Email()
         {
-            return View();
+            return View(null);
         }
     }
 }

2e7a6a8b96a40f9af4cf9600638fbab0e04b41c9
linsas <linsas0109@gmail.com>
2018-05-11 16:39:23 +0300

    Add dynamic home page slider

---
+++ Kodhier/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Kodhier.ViewModels
+{
+    public class HomeViewModel
+	{
+		public int Num { get; set; }
+
+		public string Name { get; set; }
+		public string ImagePath { get; set; }
+		public string Description { get; set; }
+
+		public decimal MinPrice { get; set; }
+	}
+}

cb99f0d47a91167a152af049a70b5d5711d5d39e
GedasFX <gedasfx@github.com>
2018-05-11 14:15:39 +0300

    Add email controller

---
+++ Kodhier/ViewModels/Admin/EmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Kodhier.Models;
+
+namespace Kodhier.ViewModels.Admin
+{
+    public class EmailViewModel
+    {
+        public string Message { get; set; }
+    }
+}

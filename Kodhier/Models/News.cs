68cc1fe41bff8456ef37b6d5224190e4ed11a030
GedasFX <gedasfx@github.com>
2018-05-15 20:55:04 +0300

    Add news management for admin

---
+++ Kodhier/Models/News.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kodhier.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Caption { get; set; }
+
+        public bool IsActive { get; set; }
+
+        [ForeignKey("Pizza")]
+        public Guid? PizzaId { get; set; }
+        public virtual Pizza Pizza { get; set; }
+    }
+}
---
+++ Kodhier/ViewModels/Admin/NewsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Kodhier.Models;
+
+namespace Kodhier.ViewModels.Admin
+{
+    public class NewsViewModel
+    {
+        public News[] Slides { get; set; }
+        public IEnumerable<Pizza> Pizzas { get; set; }
+
+        public string Title { get; set; }
+        public string Caption { get; set; }
+        public Guid? PizzaId { get; set; }
+
+        public string IsActive { get; set; }
+    }
+}

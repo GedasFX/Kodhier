886dffa90a9c303d995a4410461f54b8e7c4bc2a
GedasFX <gedasfx@github.com>
2018-05-15 23:00:37 +0300

    Display new pizzas on front page.

--- Kodhier/ViewModels/SliderViewModel.cs
+++ Kodhier/ViewModels/SliderViewModel.cs
@@ -1,17 +1,12 @@
-﻿using System;
-using Kodhier.Models;
-
-namespace Kodhier.ViewModels
+﻿namespace Kodhier.ViewModels
 {
     public class SliderViewModel
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Caption { get; set; }
 
-        public bool IsActive { get; set; }
+        public decimal Price { get; set; }
 
-        public Guid? PizzaId { get; set; }
-        public virtual Pizza Pizza { get; set; }
+        public string PizzaImgPath { get; set; }
     }
 }

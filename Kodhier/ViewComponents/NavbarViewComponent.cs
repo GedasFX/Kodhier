ba3859cfb38de1c6ae26c916277bd72572fbe611
Akvilė Bobinaitė <akvile.bobinaite@gmail.com>
2018-05-16 23:21:52 +0300

    Add Navbar ViewComponent and ViewModel for icon cart items count.

---
+++ Kodhier/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Kodhier.Data;
+using Kodhier.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Kodhier.ViewComponents
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        private readonly KodhierDbContext _context;
+        public NavbarViewComponent( KodhierDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            int quantity;
+
+            var uid = User.GetId();
+
+            quantity = _context.Orders.Where(o => o.Client.Id == uid).Where(o => !o.IsPaid).Count();
+           
+
+            return View(new NavbarViewModel(quantity));
+        }
+    }
+}
